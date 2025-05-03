172a264071e5a13593f886c3ce271a4217f3f72c
Swaroop <swaroopkrishnakr@gmail.com>
2025-05-03 21:47:41 +0530

    test

--- rental/settings.py
+++ rental/settings.py
@@ -78,16 +78,13 @@ WSGI_APPLICATION = 'rental.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {}
 
-if os.environ.get("DATABASE_URL"):
-    DATABASES['default'] = dj_database_url.config(default=os.environ["DATABASE_URL"])
-else:
-    DATABASES['default'] = {
+DATABASES = {
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-
+}
 
 
 
