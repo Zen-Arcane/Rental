3a9d81d6e94202eb4d9ce041b4ebd5cec2da8e93
Swaroop <swaroopkrishnakr@gmail.com>
2025-05-03 20:48:37 +0530

    postgresql update

--- rental/settings.py
+++ rental/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import dj_database_url
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,12 +78,17 @@ WSGI_APPLICATION = 'rental.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
+DATABASES = {}
+
+if os.environ.get("DATABASE_URL"):
+    DATABASES['default'] = dj_database_url.config(default=os.environ["DATABASE_URL"])
+else:
+    DATABASES['default'] = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+
+
 
 
 # Password validation
